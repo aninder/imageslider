c3c2e570322ea4c6061d064b0b9326dcceaeaad6
aninder <aninder@gmail.com>
2015-03-16 02:38:24 +0530

    added categories

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function(){
+
+    document.querySelector('select').addEventListener('change', function(e){
+        //alert('changed');
+        //alert(e.srcElement.selectedIndex);
+        var new_category = e.srcElement.options[e.srcElement.selectedIndex].value;
+        var images = document.getElementsByTagName('img');
+        var re=/^.*\/[0-9]{2,3}\/[0-9]{2,3}\/(\w+)\/.*/;
+        for(var i = 0 ; i < images.length ; i++) {
+            var image_url = images.item(i).src;
+            //find the current category for each link
+            var match = image_url.match(re);
+            if(match) {
+                var current_category = image_url.match(re)[1];
+                var new_image_url = image_url.replace(current_category,new_category);
+                images.item(i).src = new_image_url;
+            }
+        }
+});
+});
+
